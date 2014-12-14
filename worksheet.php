8b0ae74fa9a0f8cd16c8fa19b6f3ca348f2412c6
jyoko <jyoko@user.noreply.git.com>
2014-12-14 18:53:40 -0500

    Working on display, saving state here

--- worksheet.php
+++ worksheet.php
@@ -24,41 +24,43 @@ list($dayLong,$day,$monthLong,$month,$year) = explode(' ', date('l j F n Y'));
 <!-- Internal files -->
 
 <link rel="stylesheet" href="./worksheet.css">
-<link rel="stylesheet" media="(max-width: 640px)" href="./max640.css">
-<link rel="stylesheet" media="(min-width: 640px)" href="./min640.css">
 <link rel="stylesheet" media="(orientation: portrait)" href="./portrait.css">
 <link rel="stylesheet" media="(orientation: landscape)" href="./landscape.css">
+<link rel="stylesheet" media="(max-width: 900px)" href="./maxwidth.css">
+<link rel="stylesheet" media="(min-width: 900px)" href="./minwidth.css">
 <script src="./include/clean.js"></script>
-
-<script>
-$(function() {
-	$("#date").datepicker();
-});
-</script>
 
 </head>
 <body>
 
 <form id="header">
-<label for="date">Date</label>
-<input type="text" name="date" required id="date" placeholder="<?=$month.'/'.$day.'/'.$year?>">
 
-<label for="truck">Truck</label>
+<label>
+<span class="headerLbl">Date</span>
+<input type="text" name="date" required id="date" placeholder="<?=$month.'/'.$day.'/'.$year?>">
+</label>
+
+<label>
+<span class="headerLbl">Truck</span>
 <select name="truck" id="truck">
  <option value="518">518</option>
  <option value="1020">1020</option>
  <option value="610">610</option>
 </select>
+</label>
 
-<label for="techs">Technician(s)</label>
+<label>
+<span class="headerLbl">Technician(s)</span>
 <select multiple name="techs" id="techs">
- <option value="NAME">NAME</option>
- <option value="NAME2">NAME2</option>
- <option value="OTHER">OTHER</option>
+ <option value="NAME0">NAME0</option>
+ <option value="NAME1">NAME1</option>
 </select>
+</label>
+<input type="text" name="newTech" id="newTechBox" style="display: none;">
 
-<label for="submit"></label>
-<input type="submit" name="submit" id="next" value="Next">
+<label><input type="button" name="next0" value="Next">
+</label>
+
 </form>
 
 </body>
